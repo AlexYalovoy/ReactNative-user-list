9927baebef283b09f099bbd33e9d43c561e9ac5e
AlexYalovoy <YalovyyAlex@gmail.com>
2019-04-10 16:33:06 +0300

    feature/screens add userlist screen and mocks data

--- src/screens/Links.screen/index.js
+++ src/screens/Links.screen/index.js
@@ -1,20 +1,48 @@
 import React from 'react';
+import { View } from 'react-native';
 import { ScrollView, StyleSheet } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
+import { Image, Text } from 'react-native-elements';
+import { firstPage, secondPage, thirdPage, fourthPage } from '../../mock';
 
 import style from './style';
 
 export default class LinksScreen extends React.Component {
+  state = {
+    userList: []
+  }
+
+  makeUserList(...args) {
+    return args
+      .reduce((acc, el) =>  [...acc,...el.data], [])
+      .sort((a, b) => a.id - b.id)
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      const userList = this.makeUserList(firstPage, secondPage, thirdPage, fourthPage);
+      this.setState({ userList })
+    }, 1000);
+  }
+
   static navigationOptions = {
-    title: 'Links',
+    title: 'User list',
   };
 
   render() {
+    const { userList } = this.state;
     return (
-      <ScrollView style={styles.container}>
-        {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
-        <ExpoLinksView />
+      <ScrollView style={styles.container} contentContainerStyle={{alignItems: "stretch"}}>
+        {userList.map( el => (
+          <View key={el.id} style={styles.userContainer}>
+            <Image
+              source={{uri: el.avatar}}
+              style={styles.userAvatar}
+            />
+            <Text style={style.userName}>
+              {`${el.first_name} ${el.last_name}`}
+            </Text>
+          </View>
+        ))}
       </ScrollView>
     );
   }
--- src/screens/Links.screen/style.js
+++ src/screens/Links.screen/style.js
@@ -1,7 +1,21 @@
 export default {
-    container: {
-      flex: 1,
-      paddingTop: 15,
-      backgroundColor: '#fff',
-    },
+  container: {
+    flex: 1,
+    paddingTop: 15,
+    backgroundColor: '#fff',
+  },
+  userContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    paddingVertical: 10,
+    paddingHorizontal: 30,
+  },
+  userAvatar: {
+    width: 100,
+    height: 100,
+  },
+  userName: {
+    fontSize: 20,
+    paddingLeft: 20,
   }
+}
